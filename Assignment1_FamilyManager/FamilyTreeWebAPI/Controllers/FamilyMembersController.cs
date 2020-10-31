baa46958131ab73f7aea6e86b1356040cf68dba5
Marin <maryn777@gmail.com>
2020-10-31 17:51:04 +0100

    All required requests created but not tested(except AddAdult and AdultsOverview)

--- Assignment1_FamilyManager/FamilyTreeWebAPI/Controllers/FamilyMembersController.cs
+++ Assignment1_FamilyManager/FamilyTreeWebAPI/Controllers/FamilyMembersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using LoginExample.Data.AddFamilyMembersService;
 using LoginExample.Models.Family.Child;
+using LoginExample.Models.Family.Child.Pet;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 
@@ -45,7 +46,7 @@ namespace FamilyTreeWebAPI.Controllers
             try
             {
                 Adult added = await _familyMembersService.AddAdultAsync(adult);
-                return Created($"/{added.Id}",added); // return newly added to-do, to get the auto generated id
+                return Created($"/{added.Id}",added); // return newly added adult
             } catch (Exception e) {
                 Console.WriteLine(e);
                 return StatusCode(500, e.Message);
@@ -58,7 +59,7 @@ namespace FamilyTreeWebAPI.Controllers
         {
             try
             {
-                IList<Adult> adults = await _familyMembersService.GetListOfAdultsAsync();
+                IList<Child> adults = await _familyMembersService.GetListOfChildren();
                 return Ok(adults);
             }
             catch (Exception e)
@@ -67,6 +68,85 @@ namespace FamilyTreeWebAPI.Controllers
                 return StatusCode(500, e.Message);
             }
         }
+        [HttpPost]
+        [Route("/Children")]
+        public async Task<ActionResult<Adult>> AddChildren([FromBody] Child child) {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Child added = await _familyMembersService.AddChild(child);
+                return Created($"/{added.Id}",added); // return newly added child,
+            } catch (Exception e) {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
         
+        [HttpGet]
+        [Route("/Pets")]
+        public async Task<ActionResult<IList<Pet>>> GetPetsAsync()
+        {
+            try
+            {
+                IList<Pet> pets = await _familyMembersService.GetListOfPets();
+                return Ok(pets);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpPost]
+        [Route("/Pets")]
+        public async Task<ActionResult<Adult>> AddPet([FromBody] Pet pet) {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Pet added = await _familyMembersService.AddPet(pet);
+                return Created($"/{added.Id}",added); // return newly added child,
+            } catch (Exception e) {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+        
+        [HttpGet]
+        [Route("/Families")]
+        public async Task<ActionResult<IList<Family>>> GetFamiliesAsync()
+        {
+            try
+            {
+                IList<Family> families = await _familyMembersService.GetListOfFamilies();
+                return Ok(families);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpPost]
+        [Route("/Families")]
+        public async Task<ActionResult<Adult>> AddFamily([FromBody] Family family) {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Family added = await _familyMembersService.AddFamily(family);
+                return Created($"/{added.FamilyName}",added); // return newly added family,
+            } catch (Exception e) {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
     }
 }
