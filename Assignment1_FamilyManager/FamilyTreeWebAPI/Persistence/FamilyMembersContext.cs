83a46678e3c266cd6d8a354e907e66eec4079bd3
Marin Bilba <maryn777@gmail.com>
2020-11-23 14:00:02 +0100

    Created the familyMembersContext

---
+++ Assignment1_FamilyManager/FamilyTreeWebAPI/Persistence/FamilyMembersContext.cs
@@ -0,0 +1,38 @@
+﻿using LoginExample.Models.Family.Child;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace FamilyTreeWebAPI.Persistence
+{
+    public class FamilyMembersContext :DbContext
+    {    
+        public DbSet<Family> Families { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+ 
+        
+            optionsBuilder.UseSqlite(@"Data Source = D:\Univeristy\Projects\SEM2_DNP\RiderProjects\DNP_Assignments\Assignment1_FamilyManager\FamilyTreeWebAPI\FamilyMembersDatabase.db");
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChildInterest>()
+                .HasKey(ci => new {ci.ChildId, ci.InterestId});
+
+            modelBuilder.Entity<Family>()
+                .HasKey(fam => new {fam.StreetName, fam.HouseNumber});
+
+            modelBuilder.Entity<ChildInterest>()
+                .HasOne(ci => ci.Child)
+                .WithMany(child => child.ChildInterests)
+                .HasForeignKey(ci => ci.ChildId);
+
+            modelBuilder.Entity<ChildInterest>()
+                .HasOne(ci => ci.Interest)
+                .WithMany(i => i.ChildInterests)
+                .HasForeignKey(ci => ci.InterestId);
+        }
+    }
+}
