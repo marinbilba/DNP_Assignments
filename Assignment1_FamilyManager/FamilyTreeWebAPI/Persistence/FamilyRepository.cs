7a91859440da5aec88777ba5a1735dbb92331a72
Marin Bilba <maryn777@gmail.com>
2020-11-23 14:01:19 +0100

    Added family repository

---
+++ Assignment1_FamilyManager/FamilyTreeWebAPI/Persistence/FamilyRepository.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using FileData;
+using LoginExample.Data.AddFamilyMembersService;
+using LoginExample.Models;
+using LoginExample.Models.Family.Child;
+using LoginExample.Models.Family.Child.Pet;
+using Models;
+
+namespace FamilyTreeWebAPI.Persistence
+{
+    public class FamilyRepository : IFamilyRepository
+    {
+        private FamilyMembersContext ctx;
+
+        public FamilyRepository(FamilyMembersContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        
+        
+        public Adult AddAdult(Adult adult)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Adult> GetListAdults()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Child AddChild(Child child)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Child> GetListChildren()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Pet AddPet(Pet addPet)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Pet> GetListOfPets()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Family AddFamily(Family family)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Family> GetListOfFamilies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User ValidateUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
